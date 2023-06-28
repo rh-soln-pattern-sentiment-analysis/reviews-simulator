bf247057aa9b5e9123d10f40be609a0f3b8f4dd9
Jaya Christina <jayachristina@gmail.com>
2023-06-28 13:27:29 +0530

    edits for swaggger response

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 
 podman build -f src/main/docker/Dockerfile.jvm -t quay.io/globex-sentiment-analysis/reviews-simulator:latest .
 
-podman push quay.io/globex-sentiment-analysis/reviews-simulator:latest .
+podman push quay.io/globex-sentiment-analysis/reviews-simulator:latest
 
-podman build -f src/main/docker/Dockerfile.jvm -t quay.io/globex-sentiment-analysis/reviews-simulator:9709c1d .
+podman build -f src/main/docker/Dockerfile.jvm -t quay.io/globex-sentiment-analysis/reviews-simulator:jc-wip .
 
-podman push quay.io/globex-sentiment-analysis/reviews-simulator:9709c1d .
+podman push quay.io/globex-sentiment-analysis/reviews-simulator:jc-wip
+
--- src/main/java/org/acme/retail/order/simulator/service/ReviewsSimulatorService.java
+++ src/main/java/org/acme/retail/order/simulator/service/ReviewsSimulatorService.java
@@ -14,15 +14,13 @@ import org.slf4j.LoggerFactory;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 
 @ApplicationScoped
 public class ReviewsSimulatorService {
 
-    private static final Logger log = LoggerFactory.getLogger(ReviewsSimulatorService.class);
-
+    
     @RestClient
     ReviewsService reviewsService;
 
@@ -30,56 +28,37 @@ public class ReviewsSimulatorService {
     @Inject
     GlobexStoreService globexStoreService;
 
-    public JsonObject simulate(String catalogue) {
-        
-        
-        int count = 5;
+    public JsonObject simulate(String catalogue) {        
+        int reviewCount = 5;
         if(catalogue==null || catalogue.isEmpty()) {
             catalogue = "clothing";
         }
-        
-        createReviewsForCatalogue(catalogue, count);
-        return new JsonObject().put("result", "Generated " + count + " product reviews for catalogue:" + catalogue);
-         
-        /* if (productId != null) {
-            createReviewsForProduct(catalogue, productId, count);            
-            log.info("Complete!");
-            return new JsonObject().put("result", "Generated " + count + " orders for random customers");
-        } */
-        
-        
-        
+        List<ProductDto> productList = globexStoreService.readByCategories(catalogue, false);
+        int prodCount = productList.size() > 5 ? 5: productList.size();
+
+        createReviewsForCatalogue(productList, prodCount, reviewCount);
+        return new JsonObject().put("result", "Generated " + (prodCount * reviewCount) + " product reviews for catalogue:" + catalogue);        
     }
 
     
-    private void createReviewsForCatalogue(String categories, int count) {
-        List<ProductDto> productList = globexStoreService.readByCategories(categories, false);
-        int prodCount = productList.size() > 5 ? 5: productList.size();
+    private void createReviewsForCatalogue(List<ProductDto> productList, int prodCount, int reviewCount) {
         
-        for(int i=0; i<prodCount; i++) {
-            for(int c=0; c<count; c++) {
+        for(int i=0; i < prodCount; i++) {
+            for(int c=0; c < reviewCount; c++) {
                 
                 reviewsService.submitReview(createReview(productList.get(i)));
             }
         }
     }
 
-    /* private void createReviewsForProduct(String catalogue, String productId, int count) {
-        reviewsService.submitReview(null);
-    } */
-
-
-
     private ProductReviewDto createReview(ProductDto productDto) {
         
         ProductReviewDto review = new ProductReviewDto();
         review.setUser(ReviewsGeneratorHelper.fetchRandomUser());
         review.setProduct(productDto);
-        review.setRating(0);
+        review.setRating(5);
         review.setReviewText(ReviewsGeneratorHelper.fetchRandomReview());
         review.setTimestamp(Instant.now().toEpochMilli());
-        
-        
         return review;
         
     }
